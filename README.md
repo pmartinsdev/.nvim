b9d0a78ae3bb165559ef46381c29bbbaeb74edc6
martins20 <martins20.tech@gmail.com>
2022-09-28 16:31:14 -0300

    feat: create install file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Martins20's NVIM Setup
+
+Welcome to my nvim configuration, i hope help you with something :)
+
+## Requirements
+
+1. Unix System
+2. Vim
+3. Neo Vim
+
+## How to install ?
+
+1. Clone this repository `git clone https://github.com/martins20/.nvim.git ~/.config/nvim`
+2. Install all extensions and packages `sh ~/.config/nvim/install.sh`
+
+And done !! Happy Hacking !!
---
+++ install.sh
@@ -0,0 +1,16 @@
+# Installing Plug from vim.plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Check and Install NodeJS if it not exists into machine
+if [ ! -x "$(command -v node)" ]; then
+    curl --fail -LSs https://install-node.now.sh/latest | sh
+    export PATH="/usr/local/bin/:$PATH"
+fi
+
+# Installing all extensions from Coc
+vim -c 'CocInstall -sync coc-tsserver coc-prettier coc-json coc-html coc-go coc-pyright coc-jedi |q'
+
+#vim -c 'PlugInstall |qa!'
+
+echo "Finished to setup plugin, happy hacking !!"
