721267d0d0c0aa40b08c0f32c48fd72e1ee778d1
martins20 <martins20.tech@gmail.com>
2022-12-17 00:05:26 -0300

    add git add and git commit shortcut

--- lua/martins20/maps.lua
+++ lua/martins20/maps.lua
@@ -34,3 +34,6 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- Git
+keymap.set('', 'gaa', '<Esc>:!git add .<Return><Return>', { silent = true })
